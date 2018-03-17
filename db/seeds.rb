73e90c36a6e38593e185049a8caea3c2319f8282
Arnaud <arnaud.meneroud@gmail.com>
2018-03-17 15:59:55 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ 	Category.create(name: "Bar")
+ 	Category.create(name: "Cuisine")
+ 	Category.create(name: "Salle")
+ 	Category.create(name: "Réception")
